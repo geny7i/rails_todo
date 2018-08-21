9bd147a14951158d3f992f4fa2a12614cb4c714a
Gen Yamaguchi <>
2018-08-21 23:31:33 +0900

    fix lib path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-
+  config.eager_load_paths += %W( #{config.root}/lib )
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
